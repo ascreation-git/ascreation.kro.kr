93ceda33b02475bccb10ee16fcfec5e2341a2d83
ascreation-git <ascreation.ent@gmail.com>
2023-07-28 18:29:14 +0900

    Update

--- static/js/privacy.js
+++ static/js/privacy.js
@@ -8,16 +8,16 @@ const intersectionCallback = (entries, observer) => {
         if (target.classList.contains('intro-section')) {
             target.querySelector('.title').classList.add('animation-down');
             target.querySelector('.subtitle').classList.add('animation-down');
-            target.querySelector('.button').classList.add('animation-up');
+            target.querySelector('.button').classList.add('animation-left');
         } else if (target.classList.contains('contents-section')) {
-            target.querySelector('.contents-wrap').classList.add('animation-right');
+            target.querySelector('.contents-wrap').classList.add('animation-right-nofade');
         }
         }
     });
 };
 
 const observer = new IntersectionObserver(intersectionCallback, {
-    threshold: 0.01,
+    threshold: 0.1,
 });
 
 const introSection = document.querySelector('.intro-section');
