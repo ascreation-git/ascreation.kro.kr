cd65fdc10841238dee37f5cb6360c824a0ac55d6
ascreation-git <ascreation.ent@gmail.com>
2023-10-08 02:14:34 +0900

    Update

--- static/js/recruit.js
+++ static/js/recruit.js
@@ -6,14 +6,16 @@ const intersectionCallback = (entries, observer) => {
         if (entry.isIntersecting) {
         const target = entry.target;
         if (target.classList.contains('intro-section')) {
-            target.querySelector('.intro-subtitle').classList.add('animation-right');
-            target.querySelector('.intro-title').classList.add('animation-right');
-            target.querySelector('.recruit-icon').classList.add('animation-left');
-            target.querySelector('.intro-button').classList.add('animation-up');
+            target.querySelector('.intro-subtitle').classList.add('animation-up');
+            target.querySelector('.intro-title').classList.add('animation-up');
+            target.querySelector('.intro-arrow').classList.add('animation-fade');
+        } else if (target.classList.contains('before-contents-section')) {
+            target.querySelector('.before-contents-title').classList.add('animation-down');
+            target.querySelector('.checklist-box').classList.add('animation-right');
         } else if (target.classList.contains('contents-section')) {
-            target.querySelector('.before-contents-animated-title').classList.add('animation-left');
-            target.querySelector('.job-amount').classList.add('animation-down');
-            target.querySelector('.job-list').classList.add('animation-left');
+            target.querySelector('.contents-title').classList.add('animation-down');
+            target.querySelector('.contents-subtitle').classList.add('animation-down');
+            target.querySelector('.contents-jobs').classList.add('animation-up');
         }
         }
     });
@@ -24,11 +26,16 @@ const observer = new IntersectionObserver(intersectionCallback, {
 });
 
 const introSection = document.querySelector('.intro-section');
+const beforeContentsSection = document.querySelector('.before-contents-section');
 const contentsSection = document.querySelector('.contents-section');
 
 
 if (introSection) {
     observer.observe(introSection);
+}
+
+if (beforeContentsSection) {
+    observer.observe(beforeContentsSection);
 }
 
 if (contentsSection) {
@@ -44,35 +51,42 @@ function getCurrentDate() {
     const day = String(today.getDate()).padStart(2, '0');
     return `${year}${month}${day}`;
 }
-  
+
 function updateStatus() {
-    const elements = document.querySelectorAll('.check-available');
     const currentDate = getCurrentDate();
-  
-    const autoAmount = document.querySelector('.auto-amount');
+    const jobApplyBtns = document.querySelectorAll('.job-apply-btn');
+    const autoAvailableElements = document.querySelectorAll('.check-available[auto-available]');
+    const jobAmountElement = document.querySelector('.job-amount');
     let availableCount = 0;
-  
-    elements.forEach((element) => {
-      const availableDate = element.getAttribute('auto-available');
-  
-      if (currentDate > availableDate) {
-        element.textContent = '지원 불가';
-        element.classList.add('unavailable');
-        element.classList.remove('available');
-      } else {
-        element.textContent = '채용 중';
-        element.classList.add('available');
-        element.classList.remove('unavailable');
-        availableCount++;
-      }
+
+    // Loop through each job-apply-btn
+    jobApplyBtns.forEach((jobApplyBtn, index) => {
+        const autoAvailableValue = autoAvailableElements[index].getAttribute('auto-available');
+        const checkAvailableElement = autoAvailableElements[index];
+        
+        if (currentDate > autoAvailableValue) {
+            checkAvailableElement.classList.add('unavailable');
+            checkAvailableElement.textContent = '현재 채용이 진행 중이지 않습니다.';
+            jobApplyBtn.classList.add('job-apply-btn-disabled');
+            jobApplyBtn.disabled = true;
+            jobApplyBtn.textContent = '❌ 채용 지원 불가';
+        } else {
+            checkAvailableElement.classList.remove('unavailable');
+            checkAvailableElement.classList.add('available');
+            checkAvailableElement.textContent = '현재 채용이 진행 중입니다.';
+            jobApplyBtn.classList.remove('job-apply-btn-disabled');
+            jobApplyBtn.disabled = false;
+            jobApplyBtn.textContent = '🔍 채용 자세히 알아보기';
+            availableCount++;
+        }
     });
-  
-    if (autoAmount) {
-      autoAmount.textContent = `${availableCount}개`;
+
+    if (jobAmountElement) {
+        jobAmountElement.textContent = `${availableCount}개`;
     }
 }
-  
+
 document.addEventListener('DOMContentLoaded', updateStatus);
-  
-setInterval(updateStatus, 1000 * 60 * 60 * 24);  
+
+setInterval(updateStatus, 1000 * 60 * 60 * 24);
 /* // Check Available Recruit Script */
